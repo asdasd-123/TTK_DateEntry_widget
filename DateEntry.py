434708cdf09ca21b6e6924ce466dfc537301e225
JPJ-WM <jack.pj@live.co.uk>
2019-02-19 13:38:50 +0000

    Added docstrings

--- DateEntry.py
+++ DateEntry.py
@@ -1,8 +1,47 @@
+#   _____        _       ______       _              
+#  |  __ \      | |     |  ____|     | |             
+#  | |  | | __ _| |_ ___| |__   _ __ | |_ _ __ _   _ 
+#  | |  | |/ _` | __/ _ \  __| | '_ \| __| '__| | | |
+#  | |__| | (_| | ||  __/ |____| | | | |_| |  | |_| |
+#  |_____/ \__,_|\__\___|______|_| |_|\__|_|   \__, |
+#                                               __/ |
+#                                              |___/ 
+#
+# A ttk widget extention to allow date inputting with validation and retrieval.
+# Contains option for either grey-filling it with text that will dissappear or 
+# actuall pre-filling the date.
+#
+# =================================== 
+# Example of how to create the widget.
+# ===================================
+# root = tk.TK()
+# date1 = DateEntry(root, prefill=False, text="Enter Date Here: YYYY-MM-DD")
+# date2 = DateEntry(root, prefill=True, text="2019-02-19")
+#
+# ===================================
+# How to retrieve data from the widget.
+# ===================================
+# Get the full date as a datetime object:
+# full_date = date1.get_date()
+#
+# Year, Month, and Day:
+# year = date1.year
+# month = date1.month
+# day = date1.day
+# 
+# ===================================
+# How to check is date in box is valid.
+# ===================================
+# if date1.is_valid:
+#     print("date1 is valid")
+
+
 import tkinter.ttk as ttk
 from datetime import datetime
 
 class DateEntry(ttk.Entry):
-    def __init__(self, master, text="YYYY-MM-DD", **kwargs):
+    def __init__(self, master, prefill=False, text="YYYY-MM-DD", **kwargs):
+        """set prefill=True if the text needs to stay after clicking into the box"""
         ttk.Entry.__init__(self, master, **kwargs)
         self.master = master
         self.text = text
